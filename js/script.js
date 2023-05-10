038da5c72fa3e40abd8c8f0723b710a59d1016c6
Ry2uko <gabrielfradz@hotmail.com>
2023-05-10 09:54:23 +0800

    fix:  continent and search filter
     Added *Antartica* continent in continent filter
    - Continent filter function
    - Now uses api from *restcountries* for countries (to get continent), but still uses *flagcdn* for the flags
    - Continent & Search filter fix

--- js/script.js
+++ js/script.js
@@ -12,43 +12,66 @@ $(document).ready(function(){
   const flagsContainer = $('.flags-container');
 
   // load country data
-  $.get("https://flagcdn.com/en/codes.json", countriesData => {
-    let countryCodesShuffled = shuffle(Object.keys(countriesData));
-    let flagsCount = countryCodesShuffled.length, currFlagsCount = 0;
+  $.get("https://restcountries.com/v3.1/all", countriesData => {
+    let parsedCountriesData = parseData(countriesData);
+    let countriesDataShuffled = shuffle(parsedCountriesData);
+    let flagsCount = parsedCountriesData.length;
 
     // Load flags
-    countryCodesShuffled.forEach(countryCode => {
+    countriesDataShuffled.forEach(countryData => {
+      let countryCode = countryData.code;
+      let countryName = countryData.name;
       flagsContainer.append(`
-      <div class="flag-container" data-country="${countriesData[countryCode]}">
+      <div class="flag-container" data-country="${countryName}">
         <div class="flag-image-container-upper">
           <div class="flag-image-container">
             <img src="https://flagcdn.com/w160/${countryCode}.png" class="flag" data-code="${countryCode}">
           </div>
         </div>
         <div class="flag-country-container">
-          <span class="flag-country">${countriesData[countryCode]}</span>
+          <span class="flag-country">${countryName}</span>
         </div>
       </div>
       `);
+    });
+    
+    let activeFilters = [
+      "Asia",
+      "Africa",
+      "Antarctica",
+      "Oceania",
+      "Europe",
+      "North America",
+      "South America"
+    ];
+
+    let filteredCountries = countriesDataShuffled.filter(countryData => {
+      return activeFilters.includes(countryData.continent);
     });
 
     $('.flags-count').html(`<span class="colored"><span id="flagsCount">${flagsCount}</span> / ${flagsCount}</span> Flags Shown`);
 
     // Search input
     $('#searchCountry').on('keyup', debounce(function() {
+      // filter flags
       let searchVal = $(this).val().toLowerCase();
+      
+      filteredCountries = countriesDataShuffled.filter(country => {
+        return activeFilters.includes(country.continent) && country.name.match(new RegExp(`${searchVal}.*`, 'i'));
+      });
+      let filteredCountriesArr = filteredCountries.reduce((a, b) => {
+        a.push(b.name);
+        return a;
+      }, []);
+
       $('.flag-container').attr('class', 'flag-container hidden');
       $('.flag-container').filter(function(){
-        return $(this).attr('data-country').match(new RegExp(`${searchVal}.*`, 'i'));
+        let countryName = $(this).attr('data-country');
+        return filteredCountriesArr.includes(countryName);
       }).removeClass('hidden');
 
-      currFlagsCount = $('.flag-container:not(.hidden)').length;
-      $('#flagsCount').text(currFlagsCount);
-
-      currFlagsCount === 0 ? $('#flagsCount').css('color', '#e61e10')
-      : currFlagsCount < flagsCount ? $('#flagsCount').css('color', '#cee40d')
-      : $('#flagsCount').css('color', '#0de454');
-
+      setFlags();
+    
     }, 50));
 
     // Continent filter modal
@@ -88,9 +111,60 @@ $(document).ready(function(){
     });
 
     $('.continent-filter').on('click', function(){
-      $(this).toggleClass('active');
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        activeFilters.splice(activeFilters.indexOf($(this).find('.continent-text').text()), 1);
+      } else {
+        $(this).addClass('active');
+        activeFilters.push($(this).find('.continent-text').text())
+      }
+      
+      let searchVal = $('#searchCountry').val().toLowerCase();
+      
+      filteredCountries = countriesDataShuffled.filter(country => {
+        return activeFilters.includes(country.continent) && country.name.match(new RegExp(`${searchVal}.*`, 'i'));
+      });
+
+      let filteredCountriesArr = filteredCountries.reduce((a, b) => {
+        a.push(b.name);
+        return a;
+      }, []);
+
+      $('.flag-container').attr('class', 'flag-container hidden');
+      $('.flag-container').filter(function(){
+        return filteredCountriesArr.includes($(this).attr('data-country'));
+      }).removeClass('hidden');
+
+
+      setFlags();
     });
+
+    // set flags count
+    const setFlags = () => {
+      let totalFlags = parsedCountriesData.length;
+      // set flag count
+      let currFlagsCount = filteredCountries.length;
+      $('#flagsCount').text(currFlagsCount);
+
+      currFlagsCount === 0 ? $('#flagsCount').css('color', '#e61e10')
+      : currFlagsCount < totalFlags ? $('#flagsCount').css('color', '#cee40d')
+      : $('#flagsCount').css('color', '#0de454');
+    };
   }); 
+
+  // create a more cleaner object from countries data
+  function parseData(countriesData) {
+    const parsedCountriesData = [];
+    countriesData.forEach(countryData => {
+      let parsedCountryObj = {};
+      parsedCountryObj.name = countryData.name.common;
+      parsedCountryObj.code = countryData.cca2.toLowerCase();
+      parsedCountryObj.continent = countryData.continents[0];
+      parsedCountriesData.push(parsedCountryObj);
+    });
+
+    return parsedCountriesData;
+  };
 
   // delay execution of the filtering function until the user has finished typing
   function debounce(fn, wait) {
